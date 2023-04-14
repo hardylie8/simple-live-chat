f7f4e6710f17ab7f4e7e73b47f44ea3c296791e7
hardyliyanto <hardyliyanto@gmail.com>
2023-04-15 01:03:10 +0700

    update seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
   User.create(name: "Luke", age:19, profilePictureLink: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600')
   User.create(name: "Lulu", age:19, profilePictureLink: 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=600')
-#   Message.create(payload: "Hi.", sender:1, receiver:2)
-#   Message.create(payload: "Hello.", sender:2, receiver:1)
+  Message.create(payload: "Hi.", sender_id:1, receiver_id:2)
+  Message.create(payload: "Hello.", sender_id:2, receiver_id:1)
